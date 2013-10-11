db728f5fbb793075757b94df44de894440cf3f1a
Andrea Mambretti <m4mbr3@gmail.com>
2013-10-11 11:20:34 +0200

    implemented the menu function

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+all:
+	erlc -o Dropboxlike ../dropbox-like.idl
+	erlc main.erl
 idl: 
 	erlc -o Dropboxlike ../dropbox-like.idl 
 	
--- main.erl
+++ main.erl
@@ -1,7 +1,6 @@
 -module(main).
--export([init/0]).
+-export([init/0, menu/0]).
 
-dropboxlike_client(X) -> Ciao=1.
 
 init() -> 
     mnesia:start(),
@@ -9,8 +8,29 @@ init() ->
     orber:install([node()], [{ifr_storage_type, ram_copies}]),
     orber:start(),
     'oe_Account':'oe_register'(),
-    dropboxlike_client("A").
+    dropboxlike_client().
 
-run() ->
+menu() ->   %io:clear(),
+            io:fwrite("/*************************  DropboxLike   ***************************/\n"),
+            
+            io:fwrite("/***********  Author : Andrea Mambretti   Version 1.0   *************/ \n"),
+            io:fwrite("/********************************************************************/ \n"),
+            io:fwrite ("Select an operation:\n"),
+            io:fwrite ("\n"),
+            io:fwrite ("    subscribe\n"),
+            io:fwrite ("    remove_account\n"),
+            io:fwrite ("    login\n"),
+            io:fwrite ("    logout\n"),
+            io:fwrite ("    send_file\n"),
+            io:fwrite ("    remove_file\n"),
+            io:fwrite ("    clear\n"),
+            io:fwrite ("    dir\n"),
+            io:fwrite ("    ls\n"),
+            io:fwrite ("    help\n"),
+            io:fwrite ("    exit\n"),
+            io:fwrite ("\n").
+
+dropboxlike_client() ->
     NS = corba:string_to_object("corbaloc:iiop:localhost:1050/NameService"),
-    Dropboximpl = 'CosNaming_NamingContextExt':'resolve_str' (NS, "DBServer").
+    Dropboximpl = 'CosNaming_NamingContextExt':'resolve_str' (NS, "DBServer"),
+    Res = "Dropboxlike_Repository":isLogged(Dropboximpl,"m4mbr3", "kasjd").
